fadfa36b8ff4a1298a3b33cd2d5bd205acd29119
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-02-05 09:20:16 -0300

    calculating remaining time

--- script.js
+++ script.js
@@ -6,19 +6,44 @@ const targetElement = document.querySelector("[data-target]");
 
 const calendar = document.getElementById("date")
 
-const changeDateFormatTo = date => {
-    const [yy, mm, dd] = date.split(/-/g);
-    return `${dd}/${mm}/${yy}`;
-  };
+const changeDateFormat = date => {
+  const [yy, mm, dd] = date.split(/-/g);
+  return `${dd}/${mm}/${yy}`;
+};
 
-  targetElement.innerHTML = changeDateFormatTo(calendar.value); 
+targetElement.innerHTML = changeDateFormat(calendar.value);
 
 calendar.addEventListener('change', () => {
 
-    targetElement.innerHTML = changeDateFormatTo(calendar.value); 
+  targetElement.innerHTML = changeDateFormat(calendar.value);
 });
 
+const render = (days, hours, minutes, seconds) => {
 
+  daysElement.innerHTML = String(days).padStart("2", 0);
+  hoursElement.innerHTML = String(hours).padStart("2", 0);
+  minutesElement.innerHTML = String(minutes).padStart("2", 0);
+  secondsElement.innerHTML = String(seconds).padStart("2", 0);
+  
+};
+
+const countdown = () => {
+  const now = new Date();
+  const targetData = calendar.valueAsNumber;
+  const timeLeft = targetData - now;
+
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  console.log(targetData)
+
+
+  render(days, hours, minutes, seconds)
   
 
+}
 
+setInterval (countdown, 1000);
